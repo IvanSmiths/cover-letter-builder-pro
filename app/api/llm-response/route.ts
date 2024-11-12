dbff2e3d7fd16060fc2951af736477f26b0881f5
IvanSmiths <info@ivansmiths.com>
2024-11-12 21:18:38 +0100

    better naming

--- app/api/llm-response/route.ts
+++ app/api/llm-response/route.ts
@@ -1,17 +1,17 @@
-import { createOpenAI } from "@ai-sdk/openai";
+import { createOpenAI as createGroq } from "@ai-sdk/openai";
 import { streamText } from "ai";
 
 export async function POST(req: Request, res: Response) {
   const reqBody = await req.json();
   const prompt = reqBody.data.prompt;
 
-  const openai = createOpenAI({
+  const groq = createGroq({
     baseURL: "https://api.groq.com/openai/v1",
     apiKey: process.env.API_KEY,
   });
 
   const result = await streamText({
-    model: openai('llama-3.1-8b-instant'),
+    model: groq('llama-3.1-8b-instant'),
     prompt: prompt,
   });
 
