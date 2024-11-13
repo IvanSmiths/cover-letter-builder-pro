d4750943dfd908925ca762ee24c1211152781797
IvanSmiths <info@ivansmiths.com>
2024-11-13 14:04:48 +0100

    improve prompt

--- app/api/llm-response/route.ts
+++ app/api/llm-response/route.ts
@@ -13,7 +13,11 @@ export async function POST(req: Request, res: Response) {
 
   const result = await streamText({
     model: groq("llama-3.1-8b-instant"),
-    prompt: `Create a cover letter based on this job posting. State that I have ${yearsOfExperience} years of experience. Refer the company name and product/service if available. Just refer to it if you are sure that the company name is stated. If is not stated, use the word "your company" for the name of the company. Give me just the body of the cover letter. Don´t add any explanation, give me just the body. Also avoid the ending salutation. Fit it in one pdf page: ${prompt}`,
+    prompt: `Create a cover letter based on this job posting. State that I have ${yearsOfExperience} years of experience. 
+    Refer the company name and product/service. Refer the name or product JUST if you are absolutely sure that the company name is stated. 
+    If is not stated, use the word "your company" for the name of the company. Give me just the body of the cover letter, 
+    without the initial salutation to the recruiter and final salutation, also without signatures. 
+    Don´t add any explanation, give me just the body of the letter. Fit it in one pdf page. Answer in the job posting language. This is the job posting: ${prompt}`,
   });
 
   return result.toDataStreamResponse();
