a640c1a4c9433b240d167394bcbc87cae287a572
IvanSmiths <info@ivansmiths.com>
2024-11-14 18:48:45 +0100

    add separator

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -8,9 +8,10 @@ import { z } from "zod";
 import { YearsInput } from "@/components/Form/YearsInput";
 import FirstNameInput from "@/components/Form/FirstNameInput";
 import LastNameInput from "@/components/Form/LastNameInput";
-import { User } from "lucide-react";
+import { Building, User } from "lucide-react";
 import EmailInput from "@/components/Form/EmailInput";
 import TelephoneInput from "@/components/Form/TelephoneInput";
+import { Separator } from "@/components/ui/separator";
 
 interface ChatFormProps {
   input: string;
@@ -39,7 +40,7 @@ export function Form({
   return (
     <FormComponent {...form}>
       <form
-        className="gap-small flex flex-col"
+        className="flex flex-col gap-small"
         onSubmit={(event) => {
           event.preventDefault();
           handleSubmit(event, {
@@ -50,18 +51,23 @@ export function Form({
           });
         }}
       >
-        <div className="gap-small flex items-center">
-          <User className="w-regular h-fit" />
-          <h2 className="text-3xl font-bold">Your Information</h2>
+        <div className="flex items-center gap-small">
+          <User className="h-fit w-regular" />
+          <h2 className="text-2xl font-bold">Your Information</h2>
         </div>
-        <div className="gap-small flex">
+        <div className="flex gap-small">
           <FirstNameInput />
           <LastNameInput />
         </div>
         <YearsInput form={form} />
-        <div className="gap-small flex">
+        <div className="flex gap-small">
           <EmailInput />
           <TelephoneInput />
+        </div>
+        <Separator className="my-small" />
+        <div className="flex items-center gap-small">
+          <Building className="h-fit w-regular" />
+          <h2 className="text-2xl font-bold">Company Information</h2>
         </div>
         <PromptInput
           input={input}
