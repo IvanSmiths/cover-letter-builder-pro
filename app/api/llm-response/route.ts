40b9aca1286fdb60a742bd2fb82d094aa2442ac7
IvanSmiths <info@ivansmiths.com>
2024-11-14 23:30:34 +0100

    mod prompt by stating Recruiter if name is not defined

--- app/api/llm-response/route.ts
+++ app/api/llm-response/route.ts
@@ -14,7 +14,9 @@ export async function POST(req: Request, res: Response) {
 
   const result = await streamText({
     model: groq("llama-3.1-8b-instant"),
-    prompt: `Create a cover letter based on this job posting. Send just one cover letter, not more than one. Dont add any separators or lines. The name of the recruiter is ${recruiter}, add the name in the initial salutation. State that i have ${yearsOfExperience} years of experience in the field.
+    prompt: `Create a cover letter based on this job posting. Send just one cover letter, not more than one. Dont add any separators or lines. 
+    The name of the recruiter is ${recruiter}, add the name just in the initial salutation. If the recruiter is not stated, use the word "Recruiter". 
+    State that i have ${yearsOfExperience} years of experience in the field.
     Refer the company name and product/service. Refer the name or product JUST if you are absolutely sure that the company name is stated. 
     If is not stated, use the word "your company" for the name of the company. Give me just the body of the cover letter, 
     without final salutation and signatures. It is subdivided into three paragraphs.
