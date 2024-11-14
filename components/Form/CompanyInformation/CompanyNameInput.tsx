069764afdc2c0b7f21e880cc42ca2f741255a190
IvanSmiths <info@ivansmiths.com>
2024-11-14 22:15:14 +0100

    add full address and tooltip

--- components/Form/CompanyInformation/CompanyNameInput.tsx
+++ components/Form/CompanyInformation/CompanyNameInput.tsx
@@ -1,12 +1,19 @@
 import { useCompanyFormStore } from "@/lib/store";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import TooltipWrapper from "@/components/ui/TooltipWrapper";
 
 const CompanyNameInput = () => {
   const { companyName, setField } = useCompanyFormStore();
   return (
     <div className="w-full">
-      <Label htmlFor="companyName">Name</Label>
+      <div className="flex items-center gap-smallest">
+        <Label htmlFor="companyName">Name</Label>
+        <TooltipWrapper
+          info=" The company name will be mentioned in the cover letter body without
+            its company type (LLC, Inc, GmbH, S.r.l, to name a few) to give a more authentic feeling."
+        />
+      </div>
       <Input
         id="companyName"
         type="text"
