2610d17be94daad2429075e7a95616d94311448e
IvanSmiths <info@ivansmiths.com>
2024-11-17 20:34:29 +0100

    make text bigger

--- components/home/header/HeaderText.tsx
+++ components/home/header/HeaderText.tsx
@@ -10,11 +10,11 @@ const HeaderText = ({ title, paragraph, isHero }: HeaderProps) => {
   return (
     <div className="align-center flex flex-col items-center gap-regular pt-large md:pt-xxlarge">
       <h1
-        className={` ${isHero ? "h-full bg-gradient-to-r from-dark to-gray-800 bg-clip-text text-4xl text-transparent dark:from-light dark:to-gray-400 md:text-7xl" : "text-3xl"} text-center font-bold`}
+        className={` ${isHero ? "h-full bg-gradient-to-r from-dark to-gray-800 bg-clip-text text-4xl text-transparent dark:from-light dark:to-gray-400 md:text-7xl lg:text-8xl" : "text-3xl"} text-center font-bold`}
       >
         {title}
       </h1>
-      <p className="text-center text-darkSecondary dark:text-lightSecondary md:w-4/6">
+      <p className="text-center text-lg text-darkSecondary dark:text-lightSecondary md:w-4/6">
         {paragraph}
       </p>
       {isHero ? (
