587de00ae4ee5ff2d106ec3eb3c25f7583680a87
IvanSmiths <info@ivansmiths.com>
2024-11-17 21:42:55 +0100

    remove useless code

--- components/home/cards/Card.tsx
+++ components/home/cards/Card.tsx
@@ -13,15 +13,13 @@ interface CardProps {
 function Card({ title, description, icon, color, size, link }: CardProps) {
   const bgColor = color === "brand" ? "bg-brand" : "bg-[#1F2937]";
   const textColor = "text-white";
-  const sizeClass =
-    size === "large" ? "col-span-2 row-span-1" : "col-span-1 row-span-1";
 
   return (
     <div
-      className={`${bgColor} ${textColor} ${sizeClass} flex flex-col rounded-3xl ${size === "small" ? "flex min-h-[20rem] flex-col items-center justify-center gap-smallest p-large" : "p-regular"}`}
+      className={`${bgColor} ${textColor} ${size === "small" ? "flex min-h-[20rem] flex-col items-center justify-center gap-smallest p-large" : "p-regular"} flex flex-col rounded-3xl`}
     >
       {icon && icon}
-      <h2 className={`font-bold ${size === "small" ? "text-7xl" : "text-4xl"}`}>
+      <h2 className={`${size === "small" ? "text-7xl" : "text-4xl"} font-bold`}>
         {title}
       </h2>
       <p
