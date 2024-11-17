525037765eaa13a69994e15293ee265962d5dee9
IvanSmiths <info@ivansmiths.com>
2024-11-17 22:04:03 +0100

    improve responsiveness

--- components/home/cards/Card.tsx
+++ components/home/cards/Card.tsx
@@ -16,11 +16,11 @@ function Card({ title, description, icon, color, size, link }: CardProps) {
 
   return (
     <div
-      className={`${bgColor} ${textColor} ${size === "small" ? "items-center justify-center gap-smallest p-regular sm:p-large" : "p-regular"} flex min-h-[15rem] flex-col rounded-3xl sm:min-h-[20rem]`}
+      className={`${bgColor} ${textColor} ${size === "small" ? "min-w-[16rem] items-center justify-center gap-smallest p-regular md:min-w-[20rem]" : "p-regular"} flex min-h-[15rem] flex-col rounded-3xl sm:min-h-[20rem]`}
     >
       {icon && icon}
       <h2
-        className={`${size === "small" ? "text-5xl md:text-7xl" : "text-2xl md:text-4xl"} font-bold`}
+        className={`${size === "small" ? "text-6xl md:text-7xl" : "text-2xl md:text-4xl"} font-bold`}
       >
         {title}
       </h2>
