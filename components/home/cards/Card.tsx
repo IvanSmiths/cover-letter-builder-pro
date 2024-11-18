7cbfada3f62cd31c992fcd815b1751e2724e5eaa
IvanSmiths <info@ivansmiths.com>
2024-11-18 11:05:05 +0100

    adjust dimensions

--- components/home/cards/Card.tsx
+++ components/home/cards/Card.tsx
@@ -16,7 +16,7 @@ function Card({ title, description, icon, color, size, link }: CardProps) {
 
   return (
     <div
-      className={`${bgColor} ${textColor} ${size === "small" ? "min-w-[16rem] items-center justify-center gap-smallest border border-lightSecondary p-regular dark:border-darkSecondary md:min-w-[25rem]" : "p-regular"} flex min-h-[15rem] flex-col rounded-3xl sm:min-h-[25rem]`}
+      className={`${bgColor} ${textColor} ${size === "small" ? "min-w-[16rem] items-center justify-center gap-smallest border border-lightSecondary p-xlarge dark:border-darkSecondary md:min-w-[30rem]" : "p-regular"} flex min-h-[15rem] flex-col rounded-3xl sm:min-h-[30rem]`}
     >
       {icon && icon}
       <h2
