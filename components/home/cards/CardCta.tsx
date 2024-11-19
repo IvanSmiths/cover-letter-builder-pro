b5fb33e734ec22d979a963c571bc1c847deb5901
IvanSmiths <info@ivansmiths.com>
2024-11-19 20:50:19 +0100

    add card cta

--- components/home/cards/CardCta.tsx
+++ components/home/cards/CardCta.tsx
@@ -3,13 +3,15 @@ import { Button } from "@/components/ui/button";
 
 const CardCta = () => {
   return (
-    <div className="mt-xlarge flex h-96 w-full flex-col items-center justify-center gap-small rounded-3xl bg-brand">
-      <h4 className="text-center text-6xl font-semibold text-light">
+    <div className="3xl:h-[40rem] mt-xlarge flex h-[28rem] w-full flex-col items-center justify-center gap-small rounded-3xl bg-brand p-small">
+      <h4 className="3xl:text-8xl text-center text-4xl font-semibold text-light md:text-6xl">
         Stand Out in the <br />
         Crowd of Applicants.
       </h4>
       <Link href="/dashboard">
-        <Button size="lg">Create cover letter</Button>
+        <Button size="lg" className="3xl:text-2xl">
+          Create cover letter
+        </Button>
       </Link>
     </div>
   );
