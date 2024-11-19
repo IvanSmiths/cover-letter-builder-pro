134065ab2d5e8bb6018ca493fc4914fc9239d68e
IvanSmiths <info@ivansmiths.com>
2024-11-19 21:09:01 +0100

    add mobile navbar

--- components/home/header/Navbar.tsx
+++ components/home/header/Navbar.tsx
@@ -3,14 +3,26 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="fixed left-0 top-0 z-20 flex w-full items-center justify-between bg-light/40 bg-clip-padding px-small py-smallest backdrop-blur-xl backdrop-filter dark:bg-dark/40 md:px-medium lg:px-xlarge xl:px-xxlarge">
-      <span>Cover letter builder</span>
-      <Link prefetch={false} href="/dashboard">
-        <Button variant="outline" size="sm">
-          Go to Dashboard
-        </Button>
-      </Link>
-    </nav>
+    <>
+      <span className="fixed left-small top-small z-20 sm:hidden">
+        Cover letter builder
+      </span>
+      <nav className="fixed bottom-small left-small z-20 block sm:hidden">
+        <Link prefetch={false} href="/dashboard">
+          <Button variant="outline" size="sm">
+            Go to Dashboard
+          </Button>
+        </Link>
+      </nav>
+      <nav className="fixed left-0 top-0 z-20 hidden w-full items-center justify-between bg-light/40 bg-clip-padding px-small py-smallest backdrop-blur-xl backdrop-filter dark:bg-dark/40 sm:flex md:px-medium lg:px-xlarge xl:px-xxlarge">
+        <span>Cover letter builder</span>
+        <Link prefetch={false} href="/dashboard">
+          <Button variant="outline" size="sm">
+            Go to Dashboard
+          </Button>
+        </Link>
+      </nav>
+    </>
   );
 };
 
