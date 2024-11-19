933c612293c5f06682df9217c95d1113c5e8f4d8
IvanSmiths <info@ivansmiths.com>
2024-11-19 21:19:24 +0100

    add navbar

--- components/home/header/Navbar.tsx
+++ components/home/header/Navbar.tsx
@@ -3,26 +3,25 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <>
-      <span className="fixed left-small top-small z-20 sm:hidden">
-        Cover letter builder
-      </span>
-      <nav className="fixed bottom-small left-small z-20 block sm:hidden">
-        <Link prefetch={false} href="/dashboard">
-          <Button variant="outline" size="sm">
-            Go to Dashboard
-          </Button>
-        </Link>
-      </nav>
-      <nav className="fixed left-0 top-0 z-20 hidden w-full items-center justify-between bg-light/40 bg-clip-padding px-small py-smallest backdrop-blur-xl backdrop-filter dark:bg-dark/40 sm:flex md:px-medium lg:px-xlarge xl:px-xxlarge">
-        <span>Cover letter builder</span>
-        <Link prefetch={false} href="/dashboard">
-          <Button variant="outline" size="sm">
-            Go to Dashboard
-          </Button>
-        </Link>
-      </nav>
-    </>
+    <nav className="fixed left-0 top-0 z-20 flex w-full items-center justify-between gap-smallest bg-light/40 bg-clip-padding px-small py-smallest backdrop-blur-xl backdrop-filter dark:bg-dark/40 md:px-medium lg:px-xlarge xl:px-xxlarge">
+      <div className="flex items-center justify-center gap-smallest sm:gap-small">
+        <img
+          src="/logo.svg"
+          alt="logo"
+          height="57"
+          width="56"
+          className="w-10"
+        />
+        <span className="text-base font-semibold sm:text-xl">
+          Cover letter builder
+        </span>
+      </div>
+      <Link prefetch={false} href="/dashboard">
+        <Button variant="outline" size="sm">
+          Go to Dashboard
+        </Button>
+      </Link>
+    </nav>
   );
 };
 
