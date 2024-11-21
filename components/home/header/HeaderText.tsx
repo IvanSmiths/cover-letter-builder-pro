e85ed52465364dea2bdf39ebe782f52221784437
IvanSmiths <info@ivansmiths.com>
2024-11-22 00:50:33 +0100

    modify style

--- components/home/header/HeaderText.tsx
+++ components/home/header/HeaderText.tsx
@@ -11,21 +11,21 @@ type HeaderProps = {
 const HeaderText = ({ title, paragraph, isHero }: HeaderProps) => {
   return (
     <div
-      className={`${isHero ? "px-0 pt-large md:pt-xxlarge xl:px-xlarge" : "mt-large"} align-center relative z-10 flex flex-col items-center gap-regular`}
+      className={`${isHero ? "px-0 pt-large md:pt-xxlarge xl:px-xxxlarge" : "mt-large"} align-center relative z-10 flex flex-col items-center gap-medium`}
     >
       <h1
-        className={`${isHero ? "3xl:text-9xl h-full bg-gradient-to-r from-dark via-dark to-gray-500 bg-clip-text text-5xl text-transparent dark:from-light dark:via-light dark:to-gray-400 md:text-7xl lg:text-8xl" : "3xl:text-8xl text-6xl"} text-center font-bold`}
+        className={`${isHero ? "h-full bg-gradient-to-r from-dark via-dark to-gray-500 bg-clip-text text-5xl text-transparent dark:from-light dark:via-light dark:to-gray-400 md:text-7xl 3xl:text-9xl" : "text-6xl 3xl:text-8xl"} text-center font-bold`}
       >
         {title}
       </h1>
       <p
-        className={` ${isHero ? "font-semibold text-dark dark:text-light" : "2xl:px-xxlarge"} 3xl:text-2xl text-center text-darkSecondary dark:text-lightSecondary md:text-lg 2xl:px-large`}
+        className={` ${isHero ? "text-dark dark:text-light" : "2xl:px-xxlarge"} text-center text-darkSecondary dark:text-lightSecondary md:text-base 2xl:px-large 3xl:text-2xl`}
       >
         {paragraph}
       </p>
       {isHero ? (
         <Link prefetch={false} href="/dashboard">
-          <Button size="lg" className="3xl:text-2xl w-fit font-bold">
+          <Button size="default" className="w-fit font-bold 3xl:text-2xl">
             Create Cover Letter
           </Button>
         </Link>
