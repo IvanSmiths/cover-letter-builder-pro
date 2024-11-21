ea7912ea36a6341d82664a09e4febe28046d2191
IvanSmiths <info@ivansmiths.com>
2024-11-22 00:51:54 +0100

    modify style

--- components/home/header/HeaderText.tsx
+++ components/home/header/HeaderText.tsx
@@ -11,7 +11,7 @@ type HeaderProps = {
 const HeaderText = ({ title, paragraph, isHero }: HeaderProps) => {
   return (
     <div
-      className={`${isHero ? "px-0 pt-large md:pt-xxlarge xl:px-xxxlarge" : "mt-large"} align-center relative z-10 flex flex-col items-center gap-medium`}
+      className={`${isHero ? "px-0 pt-large md:pt-xxlarge xl:px-xlarge 2xl:px-xxxlarge" : "mt-large"} align-center relative z-10 flex flex-col items-center gap-medium`}
     >
       <h1
         className={`${isHero ? "h-full bg-gradient-to-r from-dark via-dark to-gray-500 bg-clip-text text-5xl text-transparent dark:from-light dark:via-light dark:to-gray-400 md:text-7xl 3xl:text-9xl" : "text-6xl 3xl:text-8xl"} text-center font-bold`}
