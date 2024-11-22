423cab9a4ec4691b28c20b8e4f23a1af4fa8e214
IvanSmiths <info@ivansmiths.com>
2024-11-22 22:10:10 +0100

    improve style

--- components/dashboard/Pdf/PdfStyles/germanDINNorm.ts
+++ components/dashboard/Pdf/PdfStyles/germanDINNorm.ts
@@ -10,7 +10,7 @@ export const germanDINNorm = {
   },
   headerText: {
     fontSize: 11,
-    paddingTop: 3,
+    paddingTop: 2,
   },
   headerName: {
     fontWeight: 200,
@@ -44,7 +44,7 @@ export const germanDINNorm = {
   },
   text: {
     fontSize: 11,
-    lineHeight: 1.4,
+    lineHeight: 1.3,
     fontFamily: "Helvetica",
     color: "black",
   },
