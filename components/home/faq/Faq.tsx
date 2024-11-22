c4d4183b13b25d4a2d049b9c0b5222427cfa3a65
IvanSmiths <info@ivansmiths.com>
2024-11-22 20:41:39 +0100

    improve style

--- components/home/faq/Faq.tsx
+++ components/home/faq/Faq.tsx
@@ -32,12 +32,15 @@ const Faq = () => {
       <h3 className="text-4xl font-semibold">Frequently Asked Questions</h3>
       <div className="w-full space-y-2">
         {faqData.map((faq, index) => (
-          <details key={index} className="w-full rounded-lg">
-            <summary className="cursor-pointer p-3 text-lg">
-              {faq.question}
-            </summary>
-            <p className="mt-2 pl-3">{faq.answer}</p>
-          </details>
+          <div key={index} className="w-full">
+            <details key={index} className="w-full rounded-lg">
+              <summary className="cursor-pointer p-3 text-lg">
+                {faq.question}
+              </summary>
+              <p className="mt-2 pb-3 pl-3">{faq.answer}</p>
+            </details>
+            <div className="w-full bg-lightSecondary pt-0.5 dark:bg-darkSecondary"></div>
+          </div>
         ))}
       </div>
     </div>
