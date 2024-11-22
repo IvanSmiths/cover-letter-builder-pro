12535f55f7e455df668024a7f4b4774819271170
IvanSmiths <info@ivansmiths.com>
2024-11-22 12:21:40 +0100

    improve style

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -20,7 +20,7 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-12 px-5 py-4",
+        default: "h-10 px-3 py-4",
         sm: "h-9 rounded-md px-3",
         lg: "h-16 rounded-md px-8 font-bold",
         icon: "h-10 w-10",
