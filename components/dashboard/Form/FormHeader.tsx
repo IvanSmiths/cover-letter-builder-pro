a4b1384b812aaa767f21c296599513202fb782d2
IvanSmiths <info@ivansmiths.com>
2024-11-22 12:59:47 +0100

    improve style

--- components/dashboard/Form/FormHeader.tsx
+++ components/dashboard/Form/FormHeader.tsx
@@ -9,7 +9,7 @@ function FormHeader({ icon: Icon, title }: FormHeaderProps) {
   return (
     <div className="flex items-center gap-smallest">
       <Icon className="h-fit w-small pt-0.5" />
-      <h2 className="text-2xl font-bold">{title}</h2>
+      <h2 className="text-xl font-bold">{title}</h2>
     </div>
   );
 }
--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -28,7 +28,7 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between p-small py-10 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+        "my-5 flex flex-1 items-center justify-between p-small font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
         className,
       )}
       {...props}
