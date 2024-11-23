07fec652dbffc037181c2fa4564cf774b9c18e2e
IvanSmiths <info@ivansmiths.com>
2024-11-23 20:58:57 +0100

    fix types

--- components/dashboard/Form/FormTypes.ts
+++ components/dashboard/Form/FormTypes.ts
@@ -3,21 +3,27 @@ import { ChangeEvent, FormEvent } from "react";
 
 export const personalInformationSchema = z.object({
   yearsOfExperience: z.number().min(0).max(100),
-  personalFirstName: z.string().min(1, "Enter a first name").max(100),
-  personalLastName: z.string().min(1, "Enter a last name").max(100),
+  personalFirstName: z.string().min(1, "Enter your first name").max(100),
+  personalLastName: z.string().min(1, "Enter your last name").max(100),
+  personalCity: z.string().nullable(),
+  personalZip: z.string().nullable(),
+  personalEmail: z.string().nullable(),
+  personalTelephone: z.string().nullable(),
 });
 
 export const companyInformationSchema = z.object({
-  recruiter: z.any(),
+  recruiter: z.string().nullable(),
   companyName: z.string().min(1, "Enter a company name").max(100),
+  companyFullAddress: z.string().nullable(),
+  letterSubject: z.string().min(1, "Enter the letter subject").max(100),
 });
 
 export const promptSchema = z.object({
-  prompt: z.string().min(10, "The prompt must be at least 10 characters!"),
+  prompt: z.string().min(10, "The prompt must be at least 10 characters."),
   languages: z.string({
     required_error: "Please select a language.",
   }),
-  resume: z.any(),
+  resume: z.string().nullable(),
 });
 
 export const FormSchema = z.object({
