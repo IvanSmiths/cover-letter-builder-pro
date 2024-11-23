74327e293fc8057aae420a337cf0302ce02c0829
IvanSmiths <info@ivansmiths.com>
2024-11-23 15:35:59 +0100

    mod style

--- components/home/header/SimpleInternalButton.tsx
+++ components/home/header/SimpleInternalButton.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const SimpleInternalButton = () => {
   return (
     <Link prefetch={false} href="/dashboard">
-      <button className="rounded-md bg-light px-5 py-3 text-lg font-semibold text-dark transition-all hover:bg-dark hover:text-light">
+      <button className="rounded-md bg-light px-8 py-3 text-base font-semibold text-dark transition-all hover:bg-dark hover:text-light">
         Create Cover Letter
       </button>
     </Link>
