92900212279663bff0cadf6a395b0d81103f7418
IvanSmiths <info@ivansmiths.com>
2024-11-28 16:31:02 +0100

    fix class typo

--- components/home/footer/Footer.tsx
+++ components/home/footer/Footer.tsx
@@ -2,7 +2,7 @@ import SimpleExternalButton from "@/components/home/header/SimpleExternalButton"
 
 const Footer = () => {
   return (
-    <footer className="justify-centeri flex h-60 w-full items-center justify-center">
+    <footer className="flex h-60 w-full items-center justify-center">
       <SimpleExternalButton
         link="https://www.ivansmiths.com"
         label="Powered by Ivan Smiths"
